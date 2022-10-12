e29af76a60f06a383cf66214a1522045959c311e
aboriska <aboriska@yahoo.com>
2022-10-12 03:14:09 +0300

    Redirect to Login Page

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+        All surveys
+      </h1>
     </div>
   </header>
   <main>
@@ -14,9 +16,5 @@
 </template>
 
 <script>
-export default {
-  name: "Dashboard",
-};
+export default {};
 </script>
-
-<style scoped></style>
